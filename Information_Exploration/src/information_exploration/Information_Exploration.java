785c512aa32bc106d0d542833633d4d710162a53
Andre Driedger <adriedge@gmail.com>
2017-10-20 01:17:21 -0600

    started project folder

---
+++ Information_Exploration/src/information_exploration/Information_Exploration.java
@@ -0,0 +1,19 @@
+/*
+ * Macewan University CMPT 305 Professor: Brian Brookwell
+ */
+package information_exploration;
+
+/**
+ *
+ * @author
+ */
+public class Information_Exploration {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Information_Exploration/src/information_exploration/Scientist.java
@@ -0,0 +1,12 @@
+/*
+ * Macewan University CMPT 305 Professor: Brian Brookwell
+ */
+package information_exploration;
+
+/**
+ *
+ * @author
+ */
+public class Scientist {
+    
+}
