655dee5598ddb28410be2d9ed9605579ca11c675
Stephen Doyle <32746416+stephendoyle1992@users.noreply.github.com>
2017-11-23 19:08:33 -0700

    Merged in Interface Progress

---
+++ Information_Exploration/src/information_exploration/FXMLDocumentController.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package information_exploration;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author tahmi
+ */
+public class FXMLDocumentController implements Initializable {
+    String category,textSearch;
+    ArrayList<String> array = new ArrayList<>();
+    
+    public Button search;
+    public Button back;
+    @FXML
+    private AnchorPane rootPane;
+    
+    /**
+     *for the search button it will should use the search text to run the search
+     */
+    @FXML
+    public void handleButtonSearch(){
+        textSearch = searchText.getText();
+        System.out.println("search clicked and the category is: "+ category);
+        System.out.println(textSearch+ " is being searched");
+        getHistory();
+       
+    }
+    
+    @FXML
+    public void handleBackButton(){
+        if (!array.isEmpty()){
+            array.remove((array.size())-1);
+            history.setText(array.toString());
+        }
+    }
+    /**
+     * determines what category is chosen
+     */
+    @FXML
+    public void categoryChosen(){
+        //gets the category
+        String cat = categoryBox.getSelectionModel().getSelectedItem().toString();
+        //System.out.println(category);
+        category = cat;
+        System.out.println(category);
+    }
+    
+    public void getHistory(){
+        //ArrayList<String> array = new ArrayList<>();
+        array.add(category+": "+textSearch);
+        history.setText(array.toString());
+    }
+    //@FXML
+    //private AutoCompleteTextField autoSearch;
+    
+    @FXML
+    private Label history;
+    
+    @FXML
+    private TextField searchText;
+   
+    @FXML
+    private ComboBox categoryBox;
+    ObservableList<String> categoryList = FXCollections.observableArrayList("General","Name","Prize","Gender","Year","Country");
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        categoryBox.setItems(categoryList);
+        
+                
+        
+    }    
+    
+}
--- Information_Exploration/src/information_exploration/Information_Exploration.java
+++ Information_Exploration/src/information_exploration/Information_Exploration.java
@@ -4,38 +4,37 @@
 package information_exploration;
 
 import java.net.MalformedURLException;
+import javafx.fxml.FXMLLoader;
 import java.util.List;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
  * @author adriedger
  */
-public class Information_Exploration {
+public class Information_Exploration extends Application{
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Information Exploration");
+        stage.show();
+    }
 
     /**
      * @param args the command line arguments
+     * @throws java.net.MalformedURLException
      */
     public static void main(String[] args) throws MalformedURLException, Exception {
+        launch(args);
         UndoManager undoManager = new UndoManager();
         ReadNobel process = new ReadNobel();
-        List<Laureate> laureates = process.read();
-        //for(Laureate l : laureates)
-           //l.output();
-        
-        /*
-        Command newC = new GeneralSearch(laureates, "1929");  
-        undoManager.addCommand(newC);
-        laureates = newC.execute();
-        */
-        Command newC = new CategorySearch(laureates, "country", "canada");  
-        undoManager.addCommand(newC);
-        laureates = newC.execute();
-       
-        
-        for (Laureate l : laureates)
-            l.output();
-        
-
-        
+        List<Laureate> laureates = process.read(); 
     }
 }
